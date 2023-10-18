b9deb3f331e6d19e823257e053a215e105ef7b49
Alexandre <116552630+Creepynax@users.noreply.github.com>
2023-10-18 15:10:19 +0200

    Possibilité de suivre une série

--- src/components/Follow.tsx
+++ src/components/Follow.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
+import { getFollows } from '../functions/getFollows';
+import { firestore } from "../App";
 
 function Follow(){
+
+    async function user(db: any) {
+        const user_id = localStorage.getItem('userId');
+        if (user_id == null) {
+            console.log('pas de user')
+        } else {
+            let user = await getFollows(firestore, user_id);
+            console.log("user : ", user)
+        }
+      }
+
     return(
         <div className='container'>
             <h2>
                 Voici le composant du suivre séries
             </h2>
+            <button onClick={(e) => user(firestore)}>Console log</button>
         </div>
     )
 }
---
+++ src/functions/getFollows.tsx
@@ -0,0 +1,8 @@
+import { collection, query, where, getDocs } from 'firebase/firestore/lite';
+
+export async function getFollows(db: any, user_id: string) {
+    const q = query(collection(db, "follows"), where("user_id", "==", user_id));
+    const querySnapshot = await getDocs(q);
+    const series = querySnapshot.docs.map((doc) => doc.data());
+    return series;
+}
