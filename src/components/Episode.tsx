58ec6eef0104a7c82c8cda92efcaa26cce2a8ae2
alex <alexandre.souvorov@edu.esiee-it.fr>
2023-10-19 16:22:53 +0200

    series detail

---
+++ src/components/Episode.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import '../assets/seriepage.scss';
+
+function Episode(props : any){
+    console.log(props.data)
+
+    return(
+        <div className="episode">
+        <div className="episodeHeader">
+            <img src={"https://image.tmdb.org/t/p/w200/" + props.poster_path} />
+            <h4 className="ctr">{props.data.name}</h4>
+            <span className="date">{props.data.air_date}</span>
+        </div>
+        <span className="rating">{`E${props.data.episode_number}`}</span>
+        </div>
+    )
+}
+
+export default Episode;
