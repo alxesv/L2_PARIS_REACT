3d211afa6f9fa84f4b420d44d95a26e279055bce
Alexandre <116552630+Creepynax@users.noreply.github.com>
2023-10-19 11:11:21 +0200

    Follows update

--- src/components/Serie.tsx
+++ src/components/Serie.tsx
@@ -2,30 +2,20 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { faStar, faPlus } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { addDoc, collection } from "firebase/firestore/lite";
 import { firestore } from "../App";
+import { addFollow } from '../functions/addFollow'
 
 function Series(props : any){
 
-    async function addFollow(db: any, id_serie: number) {
-        let user = localStorage.getItem("userId");
-        
-        await addDoc(collection(db, "follows"), {
-            user_id: user,
-            serie_id: id_serie,
-        });
-        alert("Série suivie");
-    }
-
     return(
         <div className="serie">
-        <Link className="serieLink" to={"/serie/" + props.data.id}>
-            {/* Faire une fonction pour mettre une image par défaut si on ne trouve pas l'image ? */}
-            <img src={"https://image.tmdb.org/t/p/w200/" + props.data.poster_path} />
-            <h4>{props.data.name}</h4>
-        </Link>
-        <span className="rating">{props.data.vote_average} <FontAwesomeIcon color="yellow" icon={faStar}/></span>
-        <button className="followSerie" onClick={() => addFollow(firestore, props.data.id)}><FontAwesomeIcon icon={faPlus}/></button>
+            <Link className="serieLink" to={"/serie/" + props.data.id}>
+                {/* Faire une fonction pour mettre une image par défaut si on ne trouve pas l'image ? */}
+                <img src={"https://image.tmdb.org/t/p/w200/" + props.data.poster_path} />
+                <h4>{props.data.name}</h4>
+            </Link>
+            <span className="rating">{props.data.vote_average} <FontAwesomeIcon color="yellow" icon={faStar}/></span>
+            <button className="followSerie" onClick={() => addFollow(firestore, props.data.id)}><FontAwesomeIcon icon={faPlus}/></button>
         </div>
     )
 }
