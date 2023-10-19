bdc966d4bc691184759089f3f22c5a1e0ac2c6b6
alex <alexandre.souvorov@edu.esiee-it.fr>
2023-10-19 11:52:47 +0200

    Calendar feature

---
+++ src/components/CalendarItem.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+function CalendarItem(props : any){
+
+    return(
+        <div className="serie">
+        <Link className="serieLink" to={"/serie/" + props.data.serie_id}>
+            <img src={"https://image.tmdb.org/t/p/w200/" + props.data.serie_backdrop} />
+            <span className="ctr">{props.data.serie_name}</span>
+        </Link>
+        <span className="rating">{`E${props.data.episode_number} S${props.data.season_number}`}</span>
+        </div>
+    )
+}
+
+export default CalendarItem;
