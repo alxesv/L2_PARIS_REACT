d9853a21d38ef7c54925ba96dfef75ab5927c6e2
Alexandre <116552630+Creepynax@users.noreply.github.com>
2023-10-19 11:30:36 +0200

    in progress

--- src/functions/showFollows.tsx
+++ src/functions/showFollows.tsx
@@ -1,11 +1,21 @@
 import { getFollows } from "./getFollows";
 import { firestore } from "../App";
 
-export async function showfollows(user_id: any) {
+const listFollows = [
+    {"user_id": "LdGVICQ2rQgWOs9xiRT4", "serie_id": 94722},
+    {"user_id": "LdGVICQ2rQgWOs9xiRT4", "serie_id": 84958},
+    {"user_id": "LdGVICQ2rQgWOs9xiRT4", "serie_id": 114461},
+    {"user_id": "LdGVICQ2rQgWOs9xiRT4", "serie_id": 233643},
+    {"user_id": "LdGVICQ2rQgWOs9xiRT4", "serie_id": 219109},
+    {"user_id": "LdGVICQ2rQgWOs9xiRT4", "serie_id": 456},
+    {"user_id": "LdGVICQ2rQgWOs9xiRT4", "serie_id": 84958},
+]
+
+export function showfollows(user_id: any) {
     if (user_id == null) {
         console.log('pas de user')
     } else {
-        let allFollows = await getFollows(firestore, user_id);
+        let allFollows = listFollows ;
         return allFollows;
     }
 }
